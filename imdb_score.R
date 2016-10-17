91358b7c24883e7b7da398aa979591fe7e9a2d92
prishni <prishni93rateria@gmail.com>
2016-10-18 03:54:44 +0530

    Update imdb_score.R

--- imdb_score.R
+++ imdb_score.R
@@ -7,7 +7,7 @@ training_data = mv[1:4000,-c(17,10,22)]
 test_data = data.frame(mv[4001:nrow(mv),-c(17,10,22)]) 
 
 
-formula1 = imdb_score ~ color + director_facebook_likes + actor_3_facebook_likes + actor_1_facebook_likes + gross + num_voted_users +cast_total_facebook_likes  + budget + actor_2_facebook_likes + aspect_ratio + movie_facebook_likes
+formula1 = imdb_score ~ color + director_facebook_likes + actor_3_facebook_likes +  actor_1_facebook_likes + gross + num_voted_users +cast_total_facebook_likes  + budget + actor_2_facebook_likes +aspect_ratio + movie_facebook_likes + num_critic_for_reviews
 
 #constructing a linear model 
 model1  = lm(formula = formula1 , data = training_data )
@@ -16,4 +16,7 @@ p = predict(model1,newdata =  test_data)
 
 #adding a column called "predicted_score" to the test_data data frame
 test_data$predicted_score = p
+q = test_data$imdb_score
+
+sum((p-q)*(p-q),na.rm = T)
 
